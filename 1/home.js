66da31ecb11f34aa66ee773ab23f97d952f55f9a
Sonickers <sonjaaehm@gmail.com>
2022-12-01 23:50:19 +0100

    Add printing the sum of three highest Elves Calories

--- 1/home.js
+++ 1/home.js
@@ -3,8 +3,11 @@ const fs = require('fs');
 const elfsStr = fs.readFileSync('./input.txt', 'utf8').split('\n\n')
 const elfsItems = elfsStr.map(elf => elf.split('\n').map(item => parseInt(item)))
 
-const elfsSums = elfsItems.map(e => e.reduce((sum, a) => sum + a, 0))
+const elfsSums = elfsItems.map(e => e.reduce((sum, a) => sum + a, 0)).sort((a, b) => a > b ? 1 : -1)
 
-const maxElf = Math.max(...elfsSums)
 
-console.log(maxElf);
+// Printing the highest Elfs Calories
+console.log(Math.max(...elfsSums))
+
+// Printing the sum of three highest Elves Calories
+console.log(elfsSums.slice(-3).reduce((sum, a) => sum + a, 0))
