29af09cb125b94390e22d6ce747904d0bbb09bd7
Sonickers <sonjaaehm@gmail.com>
2022-12-01 19:50:31 +0100

    Change variable name and delete useless function

--- 1/home.js
+++ 1/home.js
@@ -3,10 +3,8 @@ const fs = require('fs');
 const elfsStr = fs.readFileSync('./input.txt', 'utf8').split('\n\n')
 const elfsItems = elfsStr.map(elf => elf.split('\n').map(item => parseInt(item)))
 
-const something = elfsItems.map(e => e.reduce((sum, a) => sum + a, 0))
+const elfsSums = elfsItems.map(e => e.reduce((sum, a) => sum + a, 0))
 
-function indexOfBiggest(something) {
-    return max = Math.max(...something)
-}
+const maxElf = Math.max(...elfsSums)
 
-console.log(indexOfBiggest(something));
+console.log(maxElf);
