3ce80bc079532780bca09a5947b802489901cdf9
Sonickers <sonjaaehm@gmail.com>
2022-12-04 02:32:18 +0100

    Add logic for counting value of letters

--- 3/home.js
+++ 3/home.js
@@ -7,7 +7,17 @@ const dividedRucksacks = rucksacks.map(compartments => {
     const partOne = compartments.slice(0, size)
     const partOTwo = compartments.slice(size)
     return [partOne, partOTwo]
-}) 
+})
+
+const duplicates = dividedRucksacks.map(line => {
+    return checkForDuplicate(line[0], line[1])
+})
+
+const duplicateValues = duplicates.map(findValue)
+
+console.log(duplicates)
+console.log(duplicateValues)
+console.log(duplicateValues.reduce((sum, val) => sum + val, 0))
 
 function checkForDuplicate(partOne, partTwo) {
     for (let char of partOne) {
@@ -17,6 +27,8 @@ function checkForDuplicate(partOne, partTwo) {
     }
 }
 
-dividedRucksacks.forEach(line => {
-   console.log(checkForDuplicate(line[0], line[1])) 
-})
+function findValue(item) {
+    const code = item.charCodeAt(0)
+
+    return code < 97 ? code - 38 : code - 96
+}
